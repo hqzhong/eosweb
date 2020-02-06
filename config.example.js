dbf6d24d0f6c154807f7bb3c94f35153668f247d
zhonghuiquan@cmcm.com <zhonghuiquan@cmcm.com>
2020-02-06 11:30:27 +0800

    [init] yas network init

--- config.example.js
+++ config.example.js
@@ -8,7 +8,7 @@ let config = {};
 config.PROD = false;
 
 config.toInt = 10000;
-config.coin = 'EOS';
+config.coin = 'YAS';
 
 // mongo uri and options
 config.MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/EOSweb';
@@ -42,15 +42,15 @@ config.telegram = {
 
 // reserve nodes
 config.endpoints = [
-      'http://bp.cryptolions.io',
-      'https://eos.greymass.com',
-      'https://public.eosinfra.io', 
-      'http://eosbp-0.atticlab.net'
+      // 'http://bp.cryptolions.io',
+      // 'https://eos.greymass.com',
+      // 'https://public.eosinfra.io',
+      'http://iyas.top:8888'
 ];
 
 // eosjs
 config.eosConfig = {
-  chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906",
+  chainId: "ed8636abfe625d99fc9a759d49a016fd8dcae9193676a020aae2540c9fffe32f",
   keyProvider: "",
   httpEndpoint: config.endpoints[0],
   expireInSeconds: 60,
@@ -63,21 +63,21 @@ config.eosConfig = {
 };
 
 // api url for producers list
-config.customChain = 'https://nodes.get-scatter.com';
+config.customChain = 'http://iyas.top:8888';
 
 // api url for history
-config.historyChain = 'https://eos.greymass.com';
+config.historyChain = 'http://198.74.53.102:8888';
 
 // tokens api
-config.tokensAPI = 'http://api.light.xeos.me/api/account/eos/';
+config.tokensAPI = 'http://198.74.53.102:8888/api/account/eos/';
 
 config.apiV = 'v1'; // api version
 config.RAM_UPDATE = 5 * 60 * 1000; // time for ram update - /api/api.*.socket
-config.HISTORY_UPDATE = 5 * 60 * 1000; // time for stats update - /api/api.*.socket 
+config.HISTORY_UPDATE = 5 * 60 * 1000; // time for stats update - /api/api.*.socket
 config.MAX_BUFFER = 500000; // max buffer size for child processes (kb) - /crons
 config.blockUpdateTime = 900; // mainpage upades frequency  - /api/api.*.socket in ml sec
 config.offsetElementsOnMainpage = 10; // blocks on mainpage
-config.limitAsync = 30; // max threads for async.js module  
+config.limitAsync = 30; // max threads for async.js module
 config.updateTPS = 1000;
 
 // slack notifications
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,39 +6,36 @@
 const chain = 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906';
 export const environment = {
     production: false,
-    appName: 'EOSweb',
+    appName: 'YASweb',
     network: {
-        blockchain: 'eos',
-        host: 'bp.cryptolions.io',
-        port: 443,
-        protocol: 'https',
+        blockchain: 'YAS',
+        host: '45.77.40.115',
+        port: 8888,
+        protocol: 'http',
         expireInSeconds: 120,
         chainId: chain
     },
     chain: chain,
     Eos: {
-        httpEndpoint: 'https://bp.cryptolions.io',
+        httpEndpoint: 'http://iyas.top:8888',
         chainId: chain,
         verbose: false
     },
     frontConfig: {
-       coin: 'EOS',
+       coin: 'YAS',
        bp: 'bp.json',
        tokenContract: 'eosio.token',
-       totalBalance: 'EOS',
-       convertToUSD: true,
+       totalBalance: 'YAS',
+       convertToUSD: false,
        customBalance: false,
        logo: '/assets/images/eosweb.png',
        name: {
-          big: 'eos',
+          big: 'yas',
           small: 'web'
        },
-       nets: [{ name: 'Mainnet', url: 'https://eosweb.net', active: true },
-              { name: 'Jungle', url: 'https://jungle.eosweb.net', active: false },
-              { name: 'Europechain', url: 'https://xec.eosweb.net', active: false },
-              { name: 'WAX', url: 'https://wax.eosweb.net', active: false }],
+       nets: [{ name: 'Mainnet', url: 'https://iyas.top', active: true }],
        disableNets: false,
-       voteDonationAcc: 'eoswebnetbp1',
+       voteDonationAcc: 'tothemoonyas',
        disableVoteDonation: false,
        version: '2.2.8',
        producers: 1000,
@@ -54,54 +51,3 @@ export const environment = {
     }
 };
 
-/*const chain = '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4';
-export const environment = {
-    production: true,
-    appName: 'WAXweb',
-    network: {
-        blockchain: 'eos',
-        host: 'wax.cryptolions.io',
-        port: 443,
-        protocol: 'https',
-        expireInSeconds: 120,
-        chainId: chain
-    },
-    chain: chain,
-    Eos: {
-        httpEndpoint: 'https://wax.cryptolions.io',
-        chainId: chain,
-        verbose: false
-    },
-    frontConfig: {
-       coin: 'WAX',
-       bp: 'bp.json',
-       tokenContract: 'eosio.token',
-       totalBalance: 'WAX',
-       convertToUSD: true,
-       customBalance: false,
-       logo: '/assets/images/wax.png',
-       name: {
-          big: 'wax',
-          small: 'web'
-       },
-       nets: [{ name: 'Mainnet', url: 'https://eosweb.net', active: false },
-              { name: 'Jungle', url: 'https://jungle.eosweb.net', active: false },
-              { name: 'Europechain', url: 'https://xec.eosweb.net', active: false },
-              { name: 'WAX', url: 'https://wax.eosweb.net', active: true },
-              { name: 'LYNX', url: 'https://lynx.eosweb.net', active: false }],
-       disableNets: false,
-       voteDonationAcc: 'cryptolions1',
-       disableVoteDonation: false,
-       version: '1.0.0',
-       producers: 1000,
-       social: [
-         { link: 'https://github.com/orange1337/eosweb', icon: 'fa-github' },
-         { link: 'https://www.facebook.com/EOSwebnet-199076424068961', icon: 'fa-facebook' },
-         { link: 'https://www.reddit.com/user/eosweb', icon: 'fa-reddit-alien' },
-         { link: 'https://medium.com/@EoswebN', icon: 'fa-medium' },
-         { link: 'https://twitter.com/EoswebN', icon: 'fa-twitter' },
-         { link: 'https://t.me/eoswebdevchat', icon: 'fa-telegram-plane' }
-       ],
-       liveTXenable: true
-    }
-};*/
